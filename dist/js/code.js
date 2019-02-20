618793505b51ec110d849fa644caf658fa4ad0ea
Olle <yogsther@gmail.com>
2019-02-20 10:05:28 +0100

    lmao

--- dist/js/code.js
+++ dist/js/code.js
@@ -1,16 +1,30 @@
 var burger_open = false;
 var timeout;
 
-function toggle_burger(){
+function toggle_burger() {
     clearTimeout(timeout);
     var burger = document.getElementById("burger");
-        burger.style.transition = "right .35s ease";
+    burger.style.transition = "right .35s ease";
     burger_open = !burger_open;
 
-    if(burger_open) burger.style.right = "-20vw";
-        else burger.style.right = "-100vw";
     
+    var width = document.body.offsetWidth;
+    var offset = width / 1.4;
+    if(offset > 400) offset = 400;
+
+    if (burger_open) burger.style.right = -width+offset+"px";
+    else burger.style.right = "-100vw";
+
+    console.log(-width)
+
     timeout = setTimeout(() => {
         burger.style.transition = "none";
     }, 350);
 }
+
+document.body.addEventListener("click", e => {
+    var burger_detected = false;
+    for (el of e.path)
+        if (el.id == "burger" || el.id == "burger-button") burger_detected = true;
+    if (!burger_detected && burger_open) toggle_burger();
+})
